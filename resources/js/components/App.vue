02f10106f1c996a54c7d3f36df508572260e8415
Sarder Iftekhar <sarder2008@gmail.com>
2021-04-05 11:54:36 +0100

    Working initial setup

---
+++ resources/js/components/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="w-full flex justify-items-center">
+    <div class="mx-auto w-64 bg-blue-200 mt-5">
+        <h1>Hello</h1>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
